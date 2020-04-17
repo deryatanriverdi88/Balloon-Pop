7519f03d41303603ff2ff06bab9085d1802019fe
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-04-17 16:50:05 -0400

    takes the eventListener out of mainSvg, adds for needle

--- index.js
+++ index.js
@@ -2,56 +2,76 @@ let mainSvg = document.querySelector('.main-svg');
 let secondSvg = document.querySelector('.second-svg');
 let thirdSvg = document.querySelector('.third-svg');
 let fourthSvg = document.querySelector('.fourth-svg');
-let content = document.querySelector('.content')
+let content = document.querySelector('.content');
+let needle = document.querySelector('.needle');
 
-mainSvg.addEventListener('click', function(){
-	mainSvg.style.visibility = "hidden";
-	secondSvg.style.visibility = "visible";
+needle.addEventListener('click', function(){
+	setTimeout(() =>{
+		needle.style.left = "700px";
+	}, 10);
+
+	setTimeout(() =>{
+		needle.style.left = "600px";
+	}, 20);
+
+	setTimeout(() =>{
+		needle.style.left = "500px";
+	}, 30);
+
+	setTimeout(() =>{
+		needle.style.left = "450px";
+	}, 40);
+
+	setTimeout(() => {
+		mainSvg.style.visibility = "hidden";
+		secondSvg.style.visibility = "visible";
+	}, 100);
 
     setTimeout(() => {
 		thirdSvg.style.visibility = "visible";
-    },50);
+	},120);
 
     setTimeout(() => {
 		content.style.opacity = ".9";
-    }, 60);
+	}, 140);
 
 	setTimeout(() => {
 		content.style.opacity = ".8";
-    }, 80);
+	}, 160);
 
 	setTimeout(() => {
-		content.style.opacity = ".7";
-    }, 100);
+	    content.style.opacity = ".7";
+	}, 180);
 
 	setTimeout(() => {
 		content.style.opacity = ".6";
-    }, 120);
+	}, 200);
 
 	setTimeout(() => {
 		content.style.opacity = ".4";
-    }, 140);
+	}, 220);
 
 	setTimeout(() => {
 		content.style.opacity = ".3";
-    },160);
+	},240);
 
 	setTimeout(() => {
 		content.style.opacity = ".2";
-    }, 180);
+	}, 260);
 
 	setTimeout(() => {
 		content.style.opacity = ".1";
-    }, 200);
+	}, 280);
 
 	setTimeout(() => {
 		content.style.opacity = "0";
-    }, 220);
+	}, 300);
 
 	setTimeout(() =>{
 		mainSvg.style.visibility = "visible";
 	    secondSvg.style.visibility = "hidden";
 		thirdSvg.style.visibility = "hidden";
+		needle.style.left = "800px";
 		content.style.opacity = "1";
 	}, 400);
-})
+});
