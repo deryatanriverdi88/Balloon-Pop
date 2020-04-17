430381bcc0711df83daaeee39b54d8e1b0ab43ee
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-04-17 17:14:16 -0400

    adds audio, changes setTimeout seconds

--- index.js
+++ index.js
@@ -4,74 +4,77 @@ let thirdSvg = document.querySelector('.third-svg');
 let fourthSvg = document.querySelector('.fourth-svg');
 let content = document.querySelector('.content');
 let needle = document.querySelector('.needle');
+let audio = new Audio('http://soundbible.com/mp3/Balloon%20Popping-SoundBible.com-1247261379.mp3');
 
-needle.addEventListener('click', function(){
-	setTimeout(() =>{
-		needle.style.left = "700px";
-	}, 10);
+	needle.addEventListener('click', function(){
 
-	setTimeout(() =>{
-		needle.style.left = "600px";
-	}, 20);
+		setTimeout(() =>{
+			needle.style.left = "70vw";
+		}, 50);
 
-	setTimeout(() =>{
-		needle.style.left = "500px";
-	}, 30);
+		setTimeout(() =>{
+			needle.style.left = "60vw";
+		}, 70);
 
-	setTimeout(() =>{
-		needle.style.left = "450px";
-	}, 40);
+		setTimeout(() =>{
+			needle.style.left = "50vw";
+		}, 90);
 
-	setTimeout(() => {
-		mainSvg.style.visibility = "hidden";
-		secondSvg.style.visibility = "visible";
-	}, 100);
+		setTimeout(() =>{
+			needle.style.left = "45vw";
+		}, 110);
 
-    setTimeout(() => {
-		thirdSvg.style.visibility = "visible";
-	},120);
+		setTimeout(() => {
+			audio.play();
+			mainSvg.style.visibility = "hidden";
+			secondSvg.style.visibility = "visible";
+		}, 130);
 
-    setTimeout(() => {
-		content.style.opacity = ".9";
-	}, 140);
+		setTimeout(() => {
+			thirdSvg.style.visibility = "visible";
+		},150);
 
-	setTimeout(() => {
-		content.style.opacity = ".8";
-	}, 160);
+		setTimeout(() => {
+			content.style.opacity = ".9";
+		}, 170);
 
-	setTimeout(() => {
-	    content.style.opacity = ".7";
-	}, 180);
+		setTimeout(() => {
+			content.style.opacity = ".8";
+		}, 190);
 
-	setTimeout(() => {
-		content.style.opacity = ".6";
-	}, 200);
+		setTimeout(() => {
+			content.style.opacity = ".7";
+		}, 210);
 
-	setTimeout(() => {
-		content.style.opacity = ".4";
-	}, 220);
+		setTimeout(() => {
+			content.style.opacity = ".6";
+		}, 230);
 
-	setTimeout(() => {
-		content.style.opacity = ".3";
-	},240);
+		setTimeout(() => {
+			content.style.opacity = ".4";
+		}, 250);
 
-	setTimeout(() => {
-		content.style.opacity = ".2";
-	}, 260);
+		setTimeout(() => {
+			content.style.opacity = ".3";
+		},270);
 
-	setTimeout(() => {
-		content.style.opacity = ".1";
-	}, 280);
+		setTimeout(() => {
+			content.style.opacity = ".2";
+		}, 290);
 
-	setTimeout(() => {
-		content.style.opacity = "0";
-	}, 300);
+		setTimeout(() => {
+			content.style.opacity = ".1";
+		}, 300);
 
-	setTimeout(() =>{
-		mainSvg.style.visibility = "visible";
-	    secondSvg.style.visibility = "hidden";
-		thirdSvg.style.visibility = "hidden";
-		needle.style.left = "800px";
-		content.style.opacity = "1";
-	}, 400);
-});
+		setTimeout(() => {
+			content.style.opacity = "0";
+		}, 300);
+
+		setTimeout(() =>{
+			mainSvg.style.visibility = "visible";
+			secondSvg.style.visibility = "hidden";
+			thirdSvg.style.visibility = "hidden";
+			needle.style.left = "80vw";
+			content.style.opacity = "1";
+		}, 1500);
+	});
