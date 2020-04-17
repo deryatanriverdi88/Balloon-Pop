004900c72946fac574d6c2800fb2224fb2b773b7
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-04-17 00:52:33 -0400

    adds a link to js file

---
+++ index.js
@@ -0,0 +1,5 @@
+let mainSvg = document.querySelector('.main-svg');
+let secondSvg = document.querySelector('.second-svg');
+let thirdSvg = document.querySelector('.third-svg');
+let fourthSvg = document.querySelector('.fourth-svg');
+let content = document.querySelector('.content')
